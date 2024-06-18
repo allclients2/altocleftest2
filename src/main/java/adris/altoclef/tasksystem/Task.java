02d5efe0b987fb67caff1d88518d671f3e34e677
all_clients <arlo76.15.35@gmail.com>
2024-06-18 01:01:22 -0400

    try

--- src/main/java/adris/altoclef/tasksystem/Task.java
+++ src/main/java/adris/altoclef/tasksystem/Task.java
@@ -107,7 +107,6 @@ public abstract class Task {
     }
 
     protected void setDebugState(String state) {
-        AltoClef.INSTANCE.log("STATE: " + state);
         if (state == null) {
             state = "";
         }
