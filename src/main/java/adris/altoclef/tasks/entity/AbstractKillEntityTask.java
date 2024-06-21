2b86e5b20319b9ace471614f5f824499bf130e4f
allclients2 <arlo@76.15.35@gmail.com>
2024-06-21 07:45:05 -0400

    refactoring

--- src/main/java/adris/altoclef/tasks/entity/AbstractKillEntityTask.java
+++ src/main/java/adris/altoclef/tasks/entity/AbstractKillEntityTask.java
@@ -48,7 +48,7 @@ public abstract class AbstractKillEntityTask extends AbstractDoToEntityTask {
         if (!equipWeapon(mod)) {
             float hitProg = mod.getPlayer().getAttackCooldownProgress(0);
             if (hitProg >= 1 && (mod.getPlayer().isOnGround() || mod.getPlayer().getVelocity().getY() < 0 || mod.getPlayer().isTouchingWater())) {
-                LookHelper.lookAt(mod, entity.getEyePos());
+                LookHelper.lookAt(mod, entity.getEyePos(), false);
                 mod.getControllerExtras().attack(entity);
             } else {
                 LookAtPos.lookAtPos(mod, entity.getEyePos()); // Look at them
